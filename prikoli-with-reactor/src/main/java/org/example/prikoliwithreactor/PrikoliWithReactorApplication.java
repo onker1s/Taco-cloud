25eaad6a69a95b675beac684ee3c4373115a4090
onker1s <ivan.dowakin@gmail.com>
2025-03-20 21:30:34 +0300

    Spring Integration and Reactor practice

---
+++ prikoli-with-reactor/src/main/java/org/example/prikoliwithreactor/PrikoliWithReactorApplication.java
@@ -0,0 +1,13 @@
+package org.example.prikoliwithreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrikoliWithReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrikoliWithReactorApplication.class, args);
+    }
+
+}
---
+++ simple-flow/src/main/java/sia6/FileWriterIntegrationConfig.java
@@ -0,0 +1,60 @@
+package sia6;
+
+import java.io.File;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.core.GenericTransformer;
+import org.springframework.integration.dsl.IntegrationFlow;
+
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.dsl.Files;
+import org.springframework.integration.file.support.FileExistsMode;
+
+
+@Configuration
+public class FileWriterIntegrationConfig {
+
+    @Profile("xmlconfig")
+    @Configuration
+    @ImportResource("classpath:/filewriter-config.xml")
+    public static class XmlConfiguration {}
+
+    @Profile("javaconfig")
+    @Bean
+    @Transformer(inputChannel="textInChannel",
+            outputChannel="fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return text -> text.toUpperCase();
+    }
+
+    @Profile("javaconfig")
+    @Bean
+    @ServiceActivator(inputChannel="fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler =
+                new FileWritingMessageHandler(new File("src/main/resources/files"));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+        return handler;
+    }
+
+    @Profile("javadsl1")
+    @Bean
+    public IntegrationFlow fileWriterFlow() {
+        return IntegrationFlow
+                .from(MessageChannels.direct("textInChannel"))
+                .<String, String>transform(t -> t.toUpperCase())
+                .handle(Files
+                        .outboundAdapter(new File("src/main/resources/files"))
+                        .fileExistsMode(FileExistsMode.APPEND)
+                        .appendNewLine(true))
+                .get();
+    }
+
+}
